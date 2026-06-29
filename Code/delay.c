a131a968e4d63ac4ac11d89b001d4a1df7008f72
developer <dev@example.com>
2026-06-29 18:24:04 +0800

    fix: remove unused SysTick delay interrupt

--- Code/delay.c
+++ Code/delay.c
@@ -13,10 +13,14 @@ void Delay_Init(void)
 {
     /* TIMER_1MS 时钟和周期已在 SYSCFG_DL_init() 中配置
        但中断需要手动开 */
-    DL_TimerA_clearInterruptStatus(TIMER_1MS_INST, DL_TIMER_INTERRUPT_ZERO_EVENT);
-    DL_TimerA_enableInterrupt(TIMER_1MS_INST, DL_TIMER_INTERRUPT_ZERO_EVENT);
+    DL_TimerA_stopCounter(TIMER_1MS_INST);
+    DL_TimerA_clearInterruptStatus(TIMER_1MS_INST, DL_TIMERA_INTERRUPT_ZERO_EVENT);
+    DL_TimerA_enableInterrupt(TIMER_1MS_INST, DL_TIMERA_INTERRUPT_ZERO_EVENT);
+    sys_tick_ms = 0;
     NVIC_ClearPendingIRQ(TIMER_1MS_INST_INT_IRQN);
+    NVIC_SetPriority(TIMER_1MS_INST_INT_IRQN, 2);
     NVIC_EnableIRQ(TIMER_1MS_INST_INT_IRQN);
+    DL_TimerA_startCounter(TIMER_1MS_INST);
 }
 
 void delay_ms(unsigned int ms)
